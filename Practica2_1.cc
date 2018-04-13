4aead18cdd8ac7b4c8920493d491610c5ffb764a
Manuel Hernández <manherna@ucm.es>
2018-04-13 12:35:03 +0200

    Ej1 Practica 1 hecho

--- Practica2_1.cc
+++ Practica2_1.cc
@@ -1,11 +1,50 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
+#include <iostream>
+
+
+void pruebaError();
+void showTime();
 
 
 int main(int argc, char ** argv){
-
-
-return 0;
+	
+	pruebaError();
+	return 0;
 }
+
+//Ejercicio 1 
+void pruebaError(){
+	
+	int errcode = setuid(-1);
+	//La función retorna -1 cuando algo ha salido mal. EL numero de error
+	//Está guardado en errno.	
+	if(errcode == -1){
+		//Guardamos el valor de errno rapidamente, porque errno es variable del sistema
+		//y puede cambiar su valor
+		int errnosave = errno;
+		//Imprime el ultimo error guardado por el sistema (errno)
+		perror ("Error ocurred with function setuid()");
+		char error [80]; 
+		//imprimimos el error relativo a errnosave, que nos es devuelto en strerror_r
+		char * a = strerror_r (errnosave, error, 80);
+		std::cout << "Error displayed by strerror: " << a << std::endl;
+	
+	}
+	else printf("Process priviledges changed\n");
+	
+	sleep(6);
+}
+//Ejercicio 2
+void showTime() {
+
+
+
+
+
+}
+
+
