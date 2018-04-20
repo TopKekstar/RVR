47ae2eb6ab9be097d5b1de5e9ae686048be6fe7a
Usuario VMs <usuario_vms@localhost.localdomain>
2018-04-20 13:05:44 +0200

    Añadido ej 2 de la practica 2 del tema 2 de RVR :smile:

--- Practica2_3.cc
+++ Practica2_3.cc
@@ -1,12 +1,16 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <time.h>
 
 #include <iostream>
 #include <string.h>
 
 #define BUFFER_SIZE 100
 
+void returnDay(char * buf, size_t buflen);
+void returnTime(char * buf, size_t buflen);
 //El argumento 1 es la dirección ip del servidor
 //El argumento 2 es el puerto al que hacer bind
 int main (int argc, char ** argv){
@@ -41,20 +45,64 @@ int main (int argc, char ** argv){
 	
 	bind(sock, (struct sockaddr *)res->ai_addr, res->ai_addrlen);
 
-	char * buffer [BUFFER_SIZE];
+   
+	char buffer [BUFFER_SIZE];
+	char send_buff[BUFFER_SIZE];
+
 	bool close = false;
-	struct addrinfo client;
+	struct addrinfo cliente;
+	socklen_t cliente_len;
 	
 	while(!close)
 	{
 		int bytes =
-		recvfrom(sock, buffer, BUFFER_SIZE, 	
+		recvfrom(sock, &buffer, BUFFER_SIZE, 0, (struct sockaddr *) &cliente, &cliente_len); 
+		buffer[bytes] = '\n';
+
+		char host [NI_MAXHOST];
+		char serv [NI_MAXSERV];
+
+ 		getnameinfo((struct sockaddr *) &cliente, cliente_len, host, NI_MAXHOST,
+      		serv, NI_MAXSERV, NI_NUMERICHOST|NI_NUMERICSERV);
+
+  		std::cout << "Conexión desde Host: "<< host << " Puerto: " << serv<< std::endl;
+ 	 	std::cout << "Mensaje("<< bytes <<"): "<< buffer << std::endl;
+		
 	
+		if(buffer[0] == 'q'){
+			strcpy(send_buff, "Quitting...");
+			close = true;
+		}
+		else if(buffer[0] == 't')
+			returnTime(send_buff, BUFFER_SIZE);		
+		
+		else if(buffer[0] == 'd')
+			returnDay(send_buff, BUFFER_SIZE);		
+		else {
+			strcpy(send_buff, "Command not recognized.");
+		}
+  		sendto(sock, send_buff,strlen(send_buff) , 0, (struct sockaddr *) &cliente, cliente_len);
 
 	}
 
+}
 
-
-
+void returnTime(char * buffer, size_t buflen){
+	
+	time_t tAct;
+	time(&tAct);
+	
+	struct tm * timeinfo  = localtime(&tAct);
+	strftime(buffer, buflen, "%H:%M:%S\n",  timeinfo); 
 
 }
+void returnDay(char * buffer, size_t buflen){
+	
+	time_t tAct;
+	time(&tAct);
+	
+	struct tm * timeinfo  = localtime(&tAct);
+	strftime(buffer, buflen, "It's %A!, %d of %B of %G.\n", timeinfo); 
+
+}
+
