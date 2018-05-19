c5c5bf2ed616a1007a8125a805b800c1eb971826
Franlbc97 <franlbc97@gmail.com>
2018-05-19 13:13:44 +0200

    comenzado multihilo

--- Maker.sh
+++ Maker.sh
@@ -1,5 +1,5 @@
 echo Compilando p2_1
-g++ -o p2_1.kek Practica2_1.cc -lrt 
+g++ -o p2_1.kek Practica2_1.cc -lrt
 
 echo Compilando p2_2
 g++ -o p2_2.kek Practica2_2.cc
@@ -12,5 +12,7 @@ g++ -o p2_3.kek Practica2_3.cc
 echo Compilando p2_3_TCP
 g++ -o p2_3_tcp.kek Practica2_3_TCP.cc
 
-echo Compilacion Terminada
+echo Compilando p2_3_concurrente
+g++ -o p2_3_concurrente.kek Practica2_3_concurrente.cc
 
+echo Compilacion Terminada
